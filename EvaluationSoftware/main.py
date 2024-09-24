ba25db0d5e054cd32823e0dfd89a8a872bba9251
Nico Brosda <nico.brosda@rub.de>
2024-09-24 14:25:42 +0200

    New readout for arrays with a certain assignment in between array diode and measurement channel. For the latest measurements with the 128x0.5x0.5 mm^2 array the assignment is given via an Excel sheet, I also include.
    The property Analyzer.map changed to Analyzer.maps to cover multiple measurements of one object at different x or y positions.
    
    And some file moving to make the project easier to overview. All consoles can now be found in the folder Consoles/, making the old readout functionality easier to overview in the AMS_Evaluation folder.

--- EvaluationSoftware/main.py
+++ EvaluationSoftware/main.py
@@ -64,7 +64,7 @@ class Analyzer:
         self.dark_files = []
         self.norm_factor = np.ones(diode_dimension)
         self.name = ''
-        self.map = {'x': np.array([]), 'y': np.array([]), 'z': np.array([])}
+        self.maps = [{'x': np.array([]), 'y': np.array([]), 'z': np.array([]), 'position': ''}]
         self.excluded = np.full(self.diode_dimension, False)
 
     def set_measurement(self, path_to_folder, filter_criterion, file_format='.csv', blacklist=['.png', '.pdf', '.jpg']):
@@ -157,137 +157,177 @@ class Analyzer:
             self.measurement_data.append(cache)
 
     def create_map(self, overlay='ignore', inverse=[False, False]):
-        x = []
-        y = []
-        z = []
-        for data in self.measurement_data:
-            pos = data['position']
-            if None in pos or np.isnan(pos[0]) or np.isnan(pos[1]):
-                continue
-            signal = data['signal']
-            for i, column in enumerate(signal):
-                x.append(pos[0] + i * (self.diode_size[0] + self.diode_spacing[0]))
-                cache_column = []
+        self.maps = []
+
+        def mapping(position):
+            if isinstance(position, str):
+                filter_data = self.measurement_data
+            else:
+                if self.diode_dimension[1] == 1:
+                    switch = 0
+                elif self.diode_dimension[0] == 1:
+                    switch = 1
+                else:
+                    print('Under the given diode geometry the map creation for several line array positions is not '
+                          'possible.')
+                    return None
+                filter_data = [i for i in self.measurement_data if position == i['position'][switch]]
+            x = []
+            y = []
+            z = []
+            for data in filter_data:
+                pos = data['position']
+                if None in pos or np.isnan(pos[0]) or np.isnan(pos[1]):
+                    continue
+                signal = data['signal']
+                for i, column in enumerate(signal):
+                    x.append(pos[0] + i * (self.diode_size[0] + self.diode_spacing[0]))
+                    cache_column = []
+                    for j, row in enumerate(column):
+                        y.append(pos[1] + j * (self.diode_size[1] + self.diode_spacing[1]))
+                        cache_column.append(row)
+                    z.append(cache_column)
+
+            # Sort the signals in to an array with sorted and distinct position values
+            x, y, z = np.array(x), np.array(y), np.array(z)
+
+            sorting = np.argsort(x)
+            x = x[sorting]
+            z = z[sorting]
+            distinct_x = sorted(set(x))
+            distinct_y = sorted(set(y))
+            image = np.zeros((len(distinct_x), len(distinct_y)))
+
+            for i, column in enumerate(z):
                 for j, row in enumerate(column):
-                    y.append(pos[1] + j * (self.diode_size[1] + self.diode_spacing[1]))
-                    cache_column.append(row)
-                z.append(cache_column)
+                    image[distinct_x.index(x[i]), distinct_y.index(y[i * np.shape(z)[1] + j])] = row
 
-        # Sort the signals in to an array with sorted and distinct position values
-        x, y, z = np.array(x), np.array(y), np.array(z)
+            z = image.T
+            if inverse[0]:
+                z = z[::-1]
+            if inverse[1]:
+                z = z[:, ::-1]
+            if overlay == 'ignore':
+                self.maps.append({'x': np.array(distinct_x), 'y': np.array(distinct_y), 'z': z, 'position': str(position)})
+            else:
+                pass
 
-        sorting = np.argsort(x)
-        x = x[sorting]
-        z = z[sorting]
-        distinct_x = sorted(set(x))
-        distinct_y = sorted(set(y))
-        image = np.zeros((len(distinct_x), len(distinct_y)))
-
-        for i, column in enumerate(z):
-            for j, row in enumerate(column):
-                image[distinct_x.index(x[i]), distinct_y.index(y[i*np.shape(z)[1]+j])] = row
-
-        z = image.T
-        if inverse[0]:
-            z = z[::-1]
-        if inverse[1]:
-            z = z[:, ::-1]
-        if overlay == 'ignore':
-            self.map.update({'x': np.array(distinct_x), 'y': np.array(distinct_y), 'z': z})
+        # Create loop to automatize plotting of multiple maps from 1 array (x or y shifted)
+        if self.diode_dimension[1] == 1 and len(set([i['position'][0] for i in self.measurement_data])) > 1:
+            for posi in set([i['position'][0] for i in self.measurement_data]):
+                mapping(posi)
+        elif self.diode_dimension[0] == 1 and len(set([i['position'][1] for i in self.measurement_data])) > 1:
+            for posi in set([i['position'][1] for i in self.measurement_data]):
+                mapping(posi)
         else:
-            pass
+            mapping('')
 
-    def plot_map(self, save_path=None, contour=True, intensity_limits=None, ax=None, fig=None, colorbar=True,
+    def plot_map(self, save_path=None, contour=True, intensity_limits=None, ax_in=None, fig_in=None, colorbar=True,
                  *args, **kwargs):
-        if ax is None or fig is None:
-            fig, ax = plt.subplots()
-        cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white", "black", "red", "yellow"])
-        if intensity_limits is None:
-            intensity_limits = [0, np.abs(np.max(self.map['z']) * 0.9)]
-        # print(intensity_limits)
-        levels = np.linspace(intensity_limits[0], intensity_limits[1], 100)
-        if not contour:
-            # Auto-detect step width in x and y:
-            x_steps = np.array([self.map['x'][i+1] - self.map['x'][i] for i in range(np.shape(self.map['x'])[0]-1)])
-            y_steps = np.array([self.map['y'][i+1] - self.map['y'][i] for i in range(np.shape(self.map['y'])[0]-1)])
-            # Auto-detect if whitespaces should be inserted in one-direction (>1 diode and distances = diodes geometry)
-            if self.diode_dimension[0] > 1 and x_steps.std() == 0 and \
-                    x_steps.mean() == self.diode_size[0]+self.diode_spacing[0]:
-                cache_x = np.array([self.map['x'][0]])
-                for i in range(np.shape(self.map['x'])[0]):
-                    if i == 0:
-                        cache_x = np.append(cache_x, cache_x[-1]+self.diode_spacing[0]/2)
-                        cache_x = np.append(cache_x, cache_x[-1] + self.diode_size[0])
-                    else:
-                        cache_x = np.append(cache_x, cache_x[-1] + self.diode_spacing[0])
-                        cache_x = np.append(cache_x, cache_x[-1]+self.diode_size[0])
-                cache_z = []
-                for row in self.map['z'].T:
-                    cache_z.append(np.zeros_like(row))
-                    cache_z.append(row)
-                cache_z = np.array(cache_z).T
-            # Else insert +1 step in the end of the measurement and do not add white spaces
+        print(len(self.maps))
+        print([i['position'] for i in self.maps])
+        for map_el in self.maps:
+            if ax_in is None or fig_in is None:
+                fig, ax = plt.subplots()
             else:
-                cache_x = np.append(self.map['x'], self.map['x'][-1]+x_steps.mean())
-                cache_z = self.map['z']
+                fig, ax = fig_in, ax_in
+            cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white", "black", "red", "yellow"])
+            if intensity_limits is None:
+                intensity_limits = [0, np.abs(np.max(map_el['z']) * 0.9)]
+            # print(intensity_limits)
+            levels = np.linspace(intensity_limits[0], intensity_limits[1], 100)
+            if not contour:
+                # Auto-detect step width in x and y:
+                x_steps = np.array([map_el['x'][i + 1] - map_el['x'][i] for i in range(np.shape(map_el['x'])[0] - 1)])
+                y_steps = np.array([map_el['y'][i + 1] - map_el['y'][i] for i in range(np.shape(map_el['y'])[0] - 1)])
+                # Auto-detect if whitespaces should be inserted in one-direction (>1 diode and distances = diodes geometry)
+                if self.diode_dimension[0] > 1 and x_steps.std() == 0 and \
+                        x_steps.mean() == self.diode_size[0]+self.diode_spacing[0]:
+                    cache_x = np.array([map_el['x'][0]])
+                    for i in range(np.shape(map_el['x'])[0]):
+                        if i == 0:
+                            cache_x = np.append(cache_x, cache_x[-1]+self.diode_spacing[0]/2)
+                            cache_x = np.append(cache_x, cache_x[-1] + self.diode_size[0])
+                        else:
+                            cache_x = np.append(cache_x, cache_x[-1] + self.diode_spacing[0])
+                            cache_x = np.append(cache_x, cache_x[-1]+self.diode_size[0])
+                    cache_z = []
+                    for row in map_el['z'].T:
+                        cache_z.append(np.zeros_like(row))
+                        cache_z.append(row)
+                    cache_z = np.array(cache_z).T
+                # Else insert +1 step in the end of the measurement and do not add white spaces
+                else:
+                    cache_x = np.append(map_el['x'], map_el['x'][-1] + x_steps.mean())
+                    cache_z = map_el['z']
 
-            if self.diode_dimension[1] > 1 and y_steps.std() == 0 and \
-                    y_steps.mean() == self.diode_size[1]+self.diode_spacing[1]:
-                cache_y = np.array([self.map['y'][0]])
-                for i in range(np.shape(self.map['y'])[0]):
-                    if i == 0:
-                        cache_y = np.append(cache_y, cache_y[-1] + self.diode_spacing[1] / 2)
-                        cache_y = np.append(cache_y, cache_y[-1] + self.diode_size[1])
-                    else:
-                        cache_y = np.append(cache_y, cache_y[-1] + self.diode_spacing[1])
-                        cache_y = np.append(cache_y, cache_y[-1] + self.diode_size[1])
-                cache = []
-                for row in cache_z:
-                    cache.append(np.zeros_like(row))
-                    cache.append(row)
-                cache_z = np.array(cache)
+                if self.diode_dimension[1] > 1 and y_steps.std() == 0 and \
+                        y_steps.mean() == self.diode_size[1]+self.diode_spacing[1]:
+                    cache_y = np.array([map_el['y'][0]])
+                    for i in range(np.shape(map_el['y'])[0]):
+                        if i == 0:
+                            cache_y = np.append(cache_y, cache_y[-1] + self.diode_spacing[1] / 2)
+                            cache_y = np.append(cache_y, cache_y[-1] + self.diode_size[1])
+                        else:
+                            cache_y = np.append(cache_y, cache_y[-1] + self.diode_spacing[1])
+                            cache_y = np.append(cache_y, cache_y[-1] + self.diode_size[1])
+                    cache = []
+                    for row in cache_z:
+                        cache.append(np.zeros_like(row))
+                        cache.append(row)
+                    cache_z = np.array(cache)
+                else:
+                    cache_y = np.append(map_el['y'], map_el['y'][-1] + y_steps.mean())
+                    cache_z = cache_z
+
+                # print(np.shape(cache_x), np.shape(cache_y), np.shape(cache_z))
+                norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
+                color_map = ax.pcolormesh(cache_x, cache_y, cache_z, cmap=cmap, norm=norm, shading='flat')
+                norm = matplotlib.colors.Normalize(vmin=intensity_limits[0], vmax=intensity_limits[1])
+                sm = plt.cm.ScalarMappable(norm=norm, cmap=color_map.cmap)
+                sm.set_array([])
+                if colorbar:
+                    bar = fig.colorbar(sm, ax=ax, extend='max')
             else:
-                cache_y = np.append(self.map['y'], self.map['y'][-1] + y_steps.mean())
-                cache_z = cache_z
+                # color_map = ax.contourf(X, Y, Z, cmap=cmap, extend='neither', levels=levels, *args, **kwargs)
+                if np.min(map_el['z']) < intensity_limits[0] and np.max(map_el['z']) > intensity_limits[1]:
+                    color_map = ax.contourf(map_el['x'], map_el['y'], map_el['z'], cmap=cmap, extend='both', levels=levels)
+                elif np.min(map_el['z']) < intensity_limits[0]:
+                    color_map = ax.contourf(map_el['x'], map_el['y'], map_el['z'], cmap=cmap, extend='min', levels=levels)
+                elif np.max(map_el['z']) > intensity_limits[1]:
+                    color_map = ax.contourf(map_el['x'], map_el['y'], map_el['z'], cmap=cmap, extend='max', levels=levels)
+                else:
+                    color_map = ax.contourf(map_el['x'], map_el['y'], map_el['z'], cmap=cmap, extend='neither', levels=levels)
+                # '''
+                norm = matplotlib.colors.Normalize(vmin=intensity_limits[0], vmax=intensity_limits[1])
+                sm = plt.cm.ScalarMappable(norm=norm, cmap=color_map.cmap)
+                sm.set_array([])
+                if colorbar:
+                    bar = fig.colorbar(sm, ax=ax, extend='max', ticks=color_map.levels)
+            ax.set_xlabel(r'Position x (mm)')
+            ax.set_ylabel(r'Position y (mm)')
 
-            # print(np.shape(cache_x), np.shape(cache_y), np.shape(cache_z))
-            norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
-            color_map = ax.pcolormesh(cache_x, cache_y, cache_z, cmap=cmap, norm=norm, shading='flat')
-            norm = matplotlib.colors.Normalize(vmin=intensity_limits[0], vmax=intensity_limits[1])
-            sm = plt.cm.ScalarMappable(norm=norm, cmap=color_map.cmap)
-            sm.set_array([])
+            # Scale the axis true to scale
+            x_scale = ax.get_xlim()
+            y_scale = ax.get_ylim()
+            if x_scale[1] - x_scale[0] < y_scale[1] - y_scale[0]:
+                ax.set_xlim(x_scale[0] - (y_scale[1] - y_scale[0] - x_scale[1] + x_scale[0])/2,
+                            x_scale[1] + (y_scale[1] - y_scale[0] - x_scale[1] + x_scale[0])/2)
+            elif x_scale[1] - x_scale[0] > y_scale[1] - y_scale[0]:
+                ax.set_ylim(x_scale[0] - (x_scale[1] - x_scale[0] - y_scale[1] + y_scale[0])/2,
+                            x_scale[1] + (x_scale[1] - x_scale[0] - y_scale[1] + y_scale[0])/2)
+
+            # x_scale = ax.get_xlim()
+            # ax.set_xlim(x_scale[0]-(x_scale[1]-x_scale[0])*0.15, x_scale[1]+(x_scale[1]-x_scale[0])*0.15)
+
             if colorbar:
-                bar = fig.colorbar(sm, ax=ax, extend='max')
-        else:
-            # color_map = ax.contourf(X, Y, Z, cmap=cmap, extend='neither', levels=levels, *args, **kwargs)
-            if np.min(self.map['z']) < intensity_limits[0] and np.max(self.map['z']) > intensity_limits[1]:
-                color_map = ax.contourf(self.map['x'], self.map['y'], self.map['z'], cmap=cmap, extend='both', levels=levels)
-            elif np.min(self.map['z']) < intensity_limits[0]:
-                color_map = ax.contourf(self.map['x'], self.map['y'], self.map['z'], cmap=cmap, extend='min', levels=levels)
-            elif np.max(self.map['z']) > intensity_limits[1]:
-                color_map = ax.contourf(self.map['x'], self.map['y'], self.map['z'], cmap=cmap, extend='max', levels=levels)
-            else:
-                color_map = ax.contourf(self.map['x'], self.map['y'], self.map['z'], cmap=cmap, extend='neither', levels=levels)
-            # '''
-            norm = matplotlib.colors.Normalize(vmin=intensity_limits[0], vmax=intensity_limits[1])
-            sm = plt.cm.ScalarMappable(norm=norm, cmap=color_map.cmap)
-            sm.set_array([])
-            if colorbar:
-                bar = fig.colorbar(sm, ax=ax, extend='max', ticks=color_map.levels)
-        ax.set_xlabel(r'Position x (mm)')
-        ax.set_ylabel(r'Position y (mm)')
-        if colorbar:
-            bar.set_label('Measured Signal (a.u.)')
+                bar.set_label('Measured Signal (a.u.)')
 
-        save_name = self.name + '_map'
-        if contour:
-            save_name += '_contour'
-        if save_path is not None:
-            format_save(save_path=save_path, save_name=save_name)
-        if colorbar:
-            return color_map, bar
-        else:
-            return color_map, None
+            save_name = self.name + '_map' + map_el['position']
+            if contour:
+                save_name += '_contour'
+            if save_path is not None:
+                format_save(save_path=save_path, save_name=save_name)
 
     def overview(self):
         pass
